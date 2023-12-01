4dc69c15cbd7999e4b4aecb3b2a1e4286ddb42af
GnaneshwariSarala <137158564+GnaneshwariSarala@users.noreply.github.com>
2023-12-01 11:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-window.promises = [];
+document.addEventListener("DOMContentLoaded", () => {
+    const outputDiv = document.getElementById('output');
 
-// Do not change the code above this
-// add your promises to the array `promises`
+    // Function to create a promise with a random time between 1 and 5 seconds
+    function createPromise() {
+        const randomTime = Math.floor(Math.random() * 5) + 1;
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(`Resolved after ${randomTime} seconds`);
+            }, randomTime * 1000); // Converting seconds to milliseconds
+        });
+    }
+
+    // Array of promises
+    const promises = Array.from({ length: 5 }, createPromise);
+
+    // Using Promise.any to wait for the first promise to resolve
+    Promise.any(promises)
+    .then(result => {
+        // Print the result in the output div
+        const p = document.createElement('p');
+        p.textContent = result;
+        outputDiv.appendChild(p);
+    })
+    .catch(error => {
+        console.error("All promises rejected:", error);
+    });
+});
